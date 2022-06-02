7bbf4edff17c9ef1448939efab1801c630d21b00
Nyamso <msosarehema@outlook.com>
2022-06-02 14:09:43 -0700

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * this is a program
+ * that prints the size of various
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n, sizeof(char));
+	printf("Size of int: %zu byte(s)\n, sizeof(int));
+	printf("Size of long int: %zu byte(s)\n, sizeof(longint));
+	printf("Size of long long int: %zu byte(s)\n, sizeof(longlongint));
+	printf("Size of float: %zu byte(s)\n, sizeof(float));
+	return (0);
+}
