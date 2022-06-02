cf5c76efe0b068503427ae2ab560a56127554ed4
Nyamso <msosarehema@outlook.com>
2022-06-02 15:12:02 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
-/* main program
- * Script that prints titled Programming is like building a multilingual puzzles
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - program
- * return - 0 
+ * main - Entry point
  *
+ * Return: Always 0 (Success) 
  */
 
 int main(void)
