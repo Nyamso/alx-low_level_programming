a752e24dee56a01d0652b2299f4bdc9f34d9c02b
Nyamso <msosarehema@outlook.com>
2022-06-02 14:15:43 -0700

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n, sizeof(char));
-	printf("Size of int: %zu byte(s)\n, sizeof(int));
-	printf("Size of long int: %zu byte(s)\n, sizeof(longint));
-	printf("Size of long long int: %zu byte(s)\n, sizeof(longlongint));
-	printf("Size of float: %zu byte(s)\n, sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(longint));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongint));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
