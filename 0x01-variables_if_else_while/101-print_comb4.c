025894711a2d3b843599f3a9dd280b86636cfe00
Nyamso <msosarehema@outlook.com>
2022-06-05 05:33:29 -0700

    commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of three digits
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n, m, o;
+	{
+		for (o ='0'; o <='9'; o++)
+		{
+			for ( m = ( 0 + 1); m <= '9'; m++)
+			{
+				for ( n = (m + 1); n <= '9'; n++)
+				{
+					putchar(o);
+					putchar(m);
+					putchar(n);
+
+					if ( o != '7'| m != '8'| n != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations of two two-digit numbers
+ * Retturns: 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n, m;
+	int num1, num2;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			for (num1 = '0'; num1 <= '9'; num1++)
+			{
+				for (num2 = '0'; num2 <= '9'; num2++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(' ');
+					putchar(num1);
+					putchar(num2);
+
+					if (!((n == '9' && m == '8') &&(num1 == '9' && num2 == '9')))
+					{
+						putchar(,);
+						putchar( );
+					}
+				}
+				num2 = '0';
+			}
+		}
+	}
+	putchar ('\n');
+
+	retrun (0);
+}
+
