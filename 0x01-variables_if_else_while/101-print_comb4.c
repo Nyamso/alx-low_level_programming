a0f316c5bf253fe9d14d98dc3354f016347ee92b
Nyamso <msosarehema@outlook.com>
2022-06-06 12:30:42 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,13 +13,13 @@ int main(void)
 
 	for (n = 0; n < 8; n++)
 	{
-		for (m = (n + 1); m < 9; m++)
+		for (m = n + 1; m < 9; m++)
 		{
-			for (o = (m + 1); o <= 9; o++)
+			for (o = m + 1; o < 10; o++)
 			{
-				putchar((o % 10) + '0');
-				putchar((m % 10) + '0');
 				putchar((n % 10) + '0');
+				putchar((m % 10) + '0');
+				putchar((o % 10) + '0');
 
 				if (n == 7 && m == 8 && o == 9)
 					continue;
