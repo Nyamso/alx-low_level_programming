9b58e7a6a6b815903506f7f000786ec7ebccb523
Nyamso <msosarehema@outlook.com>
2022-06-07 15:05:55 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,19 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		_putchar('0')
+		_putchar('0');
 
 		for (column = 1; column <= 9; column++)
-		{		_putchar(',');
+		{
+			_putchar(',');
+			_putchar(' ');
+			
+			product = row * column;
+			
+			if (product <= 9)
 				_putchar(' ');
-
-				product = row * column;
-
-				if (product <= 9)
-					_putchar(' ');
-
-				else
-					_putchar((product / 10) + '0');
+			else
+				_putchar((product / 10) + '0');
 				
 				_putchar((product % 10) + '0');
 		}
