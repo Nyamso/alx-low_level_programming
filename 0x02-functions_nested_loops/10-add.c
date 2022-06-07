ae00c0aabbd5eff07ae994824d20a33bc9baf09f
Nyamso <msosarehema@outlook.com>
2022-06-07 15:18:13 -0700

    commit:

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * add - a function that adds two integers and
- * returns the result.
+ * add - a function that adds two integers 
  * @n: integer arguments
- * Return: 0 integer arguments
+ * @m: integer arguments
  */
 
 int add(int n, int m)
