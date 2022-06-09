be4d883331cb474cca067193b7c784cceae169a7
Nyamso <msosarehema@outlook.com>
2022-06-09 16:06:25 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,15 +2,15 @@
 
 /**
  * main - causes an infinite loop
+ * @\\o/: symbol
  * Return: 0
  */
 
 int main(void)
 {
-        int i;
+	int i;
 	
 	printf("Infinite loop incoming :(\n)");
-	
 	i = 0;
 /**
  *	while (i < 10)
@@ -19,6 +19,5 @@ int main(void)
  *	}
  */
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
