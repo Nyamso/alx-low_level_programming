ba67ad17eafe6e2972d110b5fc901d8e3f2eafd6
Nyamso <msosarehema@outlook.com>
2022-06-09 17:54:49 -0700

    commit

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -10,17 +10,13 @@ int main(void)
 {
 	int month;
 	int day;
-        int year;
-
+	int year;
+	
 	month = 1;
 	day = 1;
-        year = 2000;
-
+	year = 2000;
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	
 	day = convert_day(month, day);
-	
 	print_remaining_days(month, day, year);
-	
 	return (0);
 }
