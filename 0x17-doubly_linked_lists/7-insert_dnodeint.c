113d03ed60eb0d0a9b002cad7a873ce61e27d55e
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-15 15:46:37 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,44 +9,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *inst, *corrent = *h;
-	size_t i = 0;
+	dlistint_t *tmp = *h, *new;
 
-	inst = malloc(sizeof(dlistint_t));
-	if (inst == NULL)
-		return (NULL);
-	inst->n = n;
-	inst->next = NULL;
-	inst->prev = NULL;
-	if (*h == NULL)
-	{
-		*h = inst;
-		return (inst);
-	}
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		(*h)->prev = inst, inst->next = *h, *h = inst;
-		return (inst);
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	else
-	{
-		for (i = 1; corrent; i++)
-		{
-			if (idx == i && corrent->next != NULL)
-			{
-				inst->prev = corrent, inst->next = corrent->next;
-				corrent->next->prev = inst;
-				corrent->next = inst;
-				return (inst);
-			}
-			else if (idx == i && corrent->next == NULL)
-			{
-				corrent->next = inst, inst->prev = corrent;
-				return (inst);
-			}
-			corrent = corrent->next;
-		}
-	}
-	free(inst);
-	return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
