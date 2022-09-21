26286b7adbfd86bf06049544ca66edf0c23eeff7
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-21 15:32:42 +0200

    commit correction

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- * free_listint -  frees a listint_t list.
- * @head: pointer
- * Return: nothing
+ * free_listint - frees a linked list
+ * @head: linked list
  */
+
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *ptr;
 
-	if (!head)
+	if (head == NULL) /* account for no linked list */
 		return;
-	while (head)
+
+	while (head != NULL) /* have ptr keep track of head node and free */
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		ptr = head;
+		head = head->next; /* move to next node while ptr frees prior */
+		free(ptr);
 	}
 }
