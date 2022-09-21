e6e6f5dda295bb0e423ab9f1e3c0ff3f4a9d2c10
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-21 15:28:14 +0200

    commit correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,30 +1,47 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - deletes the node at index index
- * of a listint_t linked list.
- * @head: pointer
- * @index: is the index of the node
- * Return: address memory node or NULL.
+ * delete_nodeint_at_index - delete a node at a given position
+ * @head: pointer to head pointer of linked list
+ * @index: index to delete node
+ * Return: 1 if succeeded, or -1 if failed
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp = *head, *next;
-	unsigned int i;
 
+	unsigned int i = 0;
+	listint_t *tmp, *tmp2;
+
+	/* account for empty list */
 	if (*head == NULL)
 		return (-1);
+
+	tmp = *head;
+
+	/* account for deleting beginning node */
 	if (index == 0)
 	{
 		*head = tmp->next;
 		free(tmp);
 		return (1);
 	}
-	for (i = 0; i < index - 1 && tmp; i++)
+
+	/* iterate tmp to idx prior to idx we want to delete */
+	while (i < (index - 1) && tmp != NULL)
+	{
 		tmp = tmp->next;
-	if (tmp == NULL || tmp->next == NULL)
+		i++;
+	}
+
+	/* account for idx out of range: don't delete and return */
+	if (i != (index - 1) || tmp->next == NULL)
 		return (-1);
-	next = tmp->next->next;
-	free(tmp->next);
-	tmp->next = next;
+
+	/* link prior idx before delete */
+	tmp2 = tmp->next;
+	tmp->next = (tmp->next)->next;
+	free(tmp2);
+
 	return (1);
 }
