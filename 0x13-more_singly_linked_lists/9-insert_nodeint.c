773cda7fbd02172c237dc3a7127c6035c4e5a4cf
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-21 15:36:00 +0200

    commit correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,57 +1,53 @@
 #include "lists.h"
-/**
- * create_node - creates a new node
- * @n: int
- * Return: address memory node
- */
-listint_t *create_node(const int n)
-{
-	listint_t *n_node;
 
-	n_node = malloc(sizeof(listint_t));
-	if (n_node == NULL)
-		return (NULL);
-	n_node->n =  n;
-	n_node->next = NULL;
-	return (n_node);
-}
 /**
- * insert_nodeint_at_index - returns the nth node of a listint_t linked list.
- * @head: pointer
- * @idx: is the index of the node
- * @n: value to insert.
- * Return: address memory node or NULL.
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to head pointer of linked list
+ * @idx: index to insert new node
+ * @n: new node's data
+ * Return: address of new node, or NULL if failed
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *isrt_node, *tmp;
-	unsigned int i;
 
-	if (!head)
+	unsigned int i = 0;
+	listint_t *new_node, *tmp;
+
+	/* account for empty pointer */
+	if (head == NULL)
 		return (NULL);
-	isrt_node = create_node(n);
-	if (!isrt_node)
+
+	/* malloc and set values for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	if (!idx)
+	new_node->n = n;
+
+	/* account for idx 0 */
+	if (idx == 0)
 	{
-		isrt_node->next = *head;
-		*head = isrt_node;
-		return (isrt_node);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	else
+
+	/* iterate to 1 before nth index to insert */
+	tmp = *head;
+	while (i < (idx - 1))
 	{
-		tmp = *head;
-		for (i = 1; tmp; i++)
+		tmp = tmp->next;
+		i++;
+
+		if (tmp == NULL) /* account for idx out of range */
 		{
-			if (i == idx)
-			{
-				isrt_node->next = tmp->next;
-				tmp->next = isrt_node;
-				return (isrt_node);
-			}
-			tmp = tmp->next;
+			free(new_node);
+			return (NULL);
 		}
-
 	}
-	return (NULL);
+
+	/* link into list */
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
