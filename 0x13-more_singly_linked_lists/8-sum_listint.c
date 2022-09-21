d5bc5964ae61791d99bac349214ea941c94952d1
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-09-21 15:35:18 +0200

    commit correction

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
+
 /**
- * sum_listint -  returns the sum of all the data (n) of a
- * listint_t linked list
- * @head: pointer
- *
- * Return: sum.
+ * sum_listint - return sum of all data in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: sum, or 0 if list is empty
  */
+
 int sum_listint(listint_t *head)
 {
+
 	int sum = 0;
 
-	if (!head)
-		return (0);
-	for (; head;)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
+
 	return (sum);
 }
