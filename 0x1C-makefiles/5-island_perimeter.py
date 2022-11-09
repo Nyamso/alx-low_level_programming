2bb920f79af8ee25db2b401ada718453b6a79cc5
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-11-09 22:16:10 +0200

    correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,28 @@
-#!/sr/bin/python3
-"""
-Module Island Perimeter
-"""
+#!/usr/bin/python3
+'''Module island perimeter'''
+
+
 def island_perimeter(grid):
-""" Calculate perimeter of grid where "1" is found"""
-    p = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col] == 0:
-                    p += 1  # top
-                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    p += 1  # bottom
-                if col == 0 or grid[row][col - 1] == 0:
-                    p += 1  # left
-                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    p += 1  # right
-    return p
+    '''Funtion calculate
+    0000000000
+    0001100000
+    0001110000
+    0000110000
+    0000000000
+    '''
+
+    rows, columns = len(grid), len(grid[0])
+    perimeter = 0
+    for row in range(rows):
+        for square in range(columns):
+            if grid[row][square] == 0:
+                continue
+            if row == 0 or grid[row - 1][square] == 0:
+                perimeter += 1
+            if row == rows - 1 or grid[row + 1][square] == 0:
+                perimeter += 1
+            if square == 0 or grid[row][square - 1] == 0:
+                perimeter += 1
+            if square == columns - 1 or grid[row][square + 1] == 0:
+                perimeter += 1
+    return perimeter
